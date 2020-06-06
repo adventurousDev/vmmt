93eba96b6abbf24dd4835fa4f87ea5ee53c7fbae
hayk <naykroveg@gmail.com>
2020-06-06 20:02:36 +0200

    Add setup(installer) functionality

--- src/VM Management Tool/Settings.cs
+++ src/VM Management Tool/Settings.cs
@@ -9,9 +9,9 @@ namespace VMManagementTool
     static class Settings
     {
         //public const string OPTIMIZATION_TEMPLATE_PATH = @"..\..\..\..\testdata\Hayk_Windows 10 1507-1803-Server 2016_comp.xml";
-        public const string OPTIMIZATION_TEMPLATE_PATH = @"..\..\..\..\testdata\bwlp_Windows_10.xml";
-        //todo because of EULA the excutable needs to be downlaoded
-        //becuase it can not be distriibued with the app
+        public const string OPTIMIZATION_TEMPLATE_PATH = @"Resources\default.xml";
+        //todo because of EULA the executable needs to be downloaded
+        //because it can not be distributed with the app
         public const string SDELETE_FOLDER = @"C:\bwLehrpool\SDelete";
 
         public const bool DEBUG = true;
